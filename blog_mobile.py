6e940ed5786baea65a6861f79309f06660f93610
YeonhoShin <43157214+JoinNova@users.noreply.github.com>
2019-02-06 18:28:31 +0900

    Add files via upload

--- blog_mobile.py
+++ blog_mobile.py
@@ -13,9 +13,11 @@ while 1:
         i+=1
         browser.get('https://m.naver.com/')
         sleep(uniform(1.0, 2.5))
-        k=randint(0,11)
+        k=randint(0,26)
         sr = "백준알고리즘 "+['boj10943','난제','신용카드','문자열폭발','팰린드롬','대소문자',
-                        '카이사르','골드바흐','감소하는수','랜덤','10993','일반화학실험'][k]
+                        '카이사르','골드바흐','감소하는수','랜덤','10993','일반화학실험','크로스워드','8진수',
+                        '명령프롬프트','보물','다이나믹','파일완전삭제','좋아해','누울','fro','2의제곱','저작권',
+                        '소수찾기','rot','아','별 찍기'][k]
         #PW = "사용자비밀번호"
         elem_search = browser.find_element_by_name("query")
         elem_search.send_keys(sr)
@@ -28,13 +30,23 @@ while 1:
               '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li/div/a',
               '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[1]/div/a',
               '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[1]/div[1]/a',
-              '//*[@id="web_1"]/div/a',
-              '//*[@id="web_1"]/div/a',
+              '//*[@id="web_1"]/div/a','//*[@id="web_1"]/div/a',
               '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[3]/div[1]/a',
+              '//*[@id="web_2"]/div/a','//*[@id="web_1"]/div/a','//*[@id="web_1"]/div/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li/div/a',
+              '//*[@id="web_1"]/div/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[2]/div[1]/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[2]/div[1]/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[1]/div[1]/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[2]/div[1]/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li/div[1]/a',
+              '//*[@id="web_1"]/div/a','//*[@id="web_4"]/div/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[1]/div/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[1]/div[1]/a',
               '//*[@id="web_2"]/div/a',
-              '//*[@id="web_1"]/div/a',
-              '//*[@id="web_1"]/div/a',
-              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li/div/a'][k]
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[4]/div[1]/a',
+              '//*[@id="web_1"]/div/a','//*[@id="web_1"]/div/a',
+              '//*[@id="_ugc_review_html"]/div/div[2]/panel-list/div[1]/panel-list/div/select-contents/div/ul/li[4]/div[1]/a'][k]
         sleep(uniform(0.5, 2.5))
         browser.find_element_by_xpath(path).click()
         sleep(uniform(0.5, 2.5))
